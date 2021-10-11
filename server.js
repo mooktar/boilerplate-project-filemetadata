ff3fad62e45b5ebf547ba2910bc4940d634c34ad
mooktar <mohmouktar@gmail.com>
2021-10-11 17:43:43 +0100

    finish all features

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer')
+var path = require('path')
+var ejs = require('ejs')
 require('dotenv').config()
 
 var app = express();
+var upload = multer({})
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,10 +15,19 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post(
+  '/api/fileanalyse',
+  upload.single('upfile'),
+  (req, res, next) => {
+    res.json({
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size
+    })
+  }
+)
 
 
-
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is running ...')
 });
